02d6495c79fcc76d986124ffd26bed53262cbbaf
Rahul R S <rahul.rs@webofficeit.com>
2024-12-02 10:26:53 +0530

    [FEATURE] TYPO3 13 Compatable

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.9.99',
+            'typo3' => '12.0.0-13.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
