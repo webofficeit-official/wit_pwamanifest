696acec69d9b15573173a3ce90fbad6c2de0d261
Siva <s.sisupalan@gingco.de>
2026-09-13 18:10:41 +0200

    Update extension title, description and bump version to 1.2.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[WIT] Pwamanifest',
-    'description' => 'This extension is to create an interface where you can manage your manifest data which can used to generate a PWA.',
+    'title' => 'PWA Manifest',
+    'description' => 'Make your TYPO3 site installable and offline-ready with a Progressive Web App (PWA) manifest and service worker — faster, more mobile-friendly, better SEO.',
     'category' => 'services',
     'author' => 'Team WebofficeIT, Rahul R S',
     'author_email' => 'info@webofficeit.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.2.1',
+    'version' => '1.2.2',
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.0-14.9.99',
